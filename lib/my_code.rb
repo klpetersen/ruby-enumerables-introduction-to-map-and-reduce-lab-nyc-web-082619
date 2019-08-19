a445df37798927d40dc968e81053d18da18d6ba6
Kayla Petersen <petersen1994@gmail.com>
2019-08-19 14:31:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,10 +48,10 @@ def reduce_to_all_true (source_array)
   while i < source_array.length do 
     if source_array[i] == true
       source_array[i] = true
-      i += 1
     else 
       source_array[i] = false
     end 
+    i += 1
   end  
 end
  
